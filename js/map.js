43dbafb095cd133fcba983bc6b027edc6cbe19a2
Tomas Svedlund <tomas.svedlund@live.se>
2025-04-28 16:48:23 +0200

    small changes

--- js/map.js
+++ js/map.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Initiera kartan och centrera den på Karlstad
     var map = L.map("map").setView([59.3793, 13.5036], 6); // [Lat, Lng], zoomnivå
+    
+    map.scrollWheelZoom.disable()
 
     // Lägg till OpenStreetMap som kartlager
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
         subdomains: 'abcd',
-        maxZoom: 19
+        maxZoom: 19,
     }).addTo(map);
 
     // Lägg till en pinpoint (marker) på Karlstad
